61b56c814be888d9bb9881aada375e4653f21dbd
gabriellichacz <gabriellichacz@gmail.com>
2022-08-27 14:00:35 +0200

    First commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1> About as page </h1>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheWelcome from '@/components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <h1> Home page </h1>
+    </div>
+  </main>
+</template>
