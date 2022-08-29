3bdf682fdd098bc2fb24cda489636e6aa3bf4ca5
gabriellichacz <gabriellichacz@gmail.com>
2022-08-29 23:47:44 +0200

    Aplikacja todo skonczona - zrobic api do zapisu danych + poprawic layout

--- src/views/ToDo.vue
+++ src/views/ToDo.vue
@@ -1,28 +1,51 @@
 <script>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 
 export default {
   setup() {
-    // declaring data - curly brackets mean one array
-    const todos = ref([
-        {
-            text: "First todo",
-            createdAt: "28-08-2022"
-        },
-        {
-            text: "Second todo",
-            createdAt: "29-08-2022"
-        },
-        {
-            text: "Third todo",
-            createdAt: "30-08-2022"
+    // declaring data
+
+    // todos data curly brackets mean one array
+    const todos = reactive([]);
+    
+    // new todo creation
+    const todoText = ref("");
+
+    function addTodo() {
+        todos.unshift({ // unshift adds data to an existing array at the beginning
+            text: todoText.value, // because the data is const todoText = {value: ""}
+            createdAt: new Date(),
+            done: false,
+        });
+
+        todoText.value = "";
+    };
+
+    function markAsDone(index) {
+        todos[index].done = true;
+    }
+
+    function markAsUnDone(index) {
+        todos[index].done = false;
+    }
+
+    function removeTodo(index) {
+        // Default confirmation
+        if (!confirm("Are you sure?")) {
+            return;
         }
-        
-    ])
+        todos.splice(index, 1) // Deleting todos[index]
+    }
+
 
     // returning data to the page
     return {
-        todos
+        todos,
+        todoText,
+        addTodo,
+        markAsDone,
+        markAsUnDone,
+        removeTodo,
     }
   },
 }
@@ -38,26 +61,30 @@ export default {
                     
                     <!-- Todo item -->
                     <div v-for="(todo, index) in todos"
-                        class="p-8 bg-purple-200 shadow-md rounded flex items-center justify-between">
+                        class="p-8 bg-purple-200 shadow-md rounded flex items-center justify-between"
+                        :class="{'bg-green-200': todo.done}">
                         <div>
-                            <div class="text-purple-800"> {{ todo.text }} </div>
-                            <div class="text-purple-800 text-sm"> {{ todo.createdAt }} </div>
+                            <div class="text-purple-800 mb-5"> {{ todo.text }} </div>
+                            <div class="text-purple-800 text-sm"> {{ todo.createdAt.toString() }} </div>
                         </div>
                         <div class="space-x-2">
-                            <button class="px-2 text-red-600" title="Remove todo">
+                            <button @click="removeTodo(index)" 
+                                class="px-2 text-red-600" title="Remove todo">
                                 &times;
                             </button>
-                            <button class="px-2 text-red-600" title="Mark as done">
+                            <button v-if="!todo.done" @click="markAsDone(index)"
+                                class="px-2 text-green-600" title="Mark as done">
                                 &check;
                             </button>
-                            <button class="px-2 text-red-600" title="Mark as undone">
+                            <button v-else @click="markAsUnDone(index)" 
+                                class="px-2 text-yellow-600" title="Mark as undone">
                                 &#8630;
                             </button>
                         </div>
                     </div>
                     <!-- end of Todo item -->
 
-                    <div class="px-8 py-10 bg-purple-200 text-purple-800 shadow-md rounded text-sm">
+                    <div v-if="todos.length == 0" class="px-8 py-10 bg-purple-200 text-purple-800 shadow-md rounded text-sm">
                         You don't have any taks to do.
                     </div>
 
@@ -66,7 +93,9 @@ export default {
                 <div class="col-span-6">
                     <div class="p-8 bg-purple-200 text-purple-800 shadow-md rounded">
                         <h2 class="text-xl"> Add a todo </h2>
-                        <input type="text" class="p-2 mt-4 bg-purple-100 text-purple-800 border rounded w-full">
+                        <!-- enter key adds todo-->
+                        <input type="text" v-model="todoText" @keydown.enter="addTodo"
+                            class="p-2 mt-4 bg-purple-100 text-purple-800 border rounded w-full">
                     </div>
                 </div>
 
